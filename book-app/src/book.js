e36b7f28fc07cdb800d98d0fc0c1a94b4cd809d3
Marienoir <okosunmaryeghonghon@gmail.com>
2023-02-05 20:39:34 +0100

    Initial commit for the book app

--- book-app/src/book.js
+++ book-app/src/book.js
@@ -1,20 +1,212 @@
-import "./App.css";
-import axios from "axios";
-function Book() {
-    console.log('hghffyggghj');
-//   const getUser = async () => {
-//     try {
-//       const response = await axios.get(
-//         "https://www.googleapis.com/books/v1/volumes?q=harry+potter&callback=handleResponse"
-//       );
-//       console.log(response);
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   };
-  return <div className="App">
-    <h1>jkjhkjh</h1>
-  </div>
-}
+import React from "react";
+import "./style.css";
+import { useTranslation } from "react-i18next";
+
+const Book = () => {
+  const { t } = useTranslation();
+  return (
+    <div>
+      <div className="nav-bar">
+        <h1>Reading List</h1>
+        <select>
+          <option selected value="en">
+            English (en)
+          </option>
+          <option value="es">Spanish (es)</option>
+          <option value="cs">Czech (cs)</option>
+          <option value="fr">French (fr)</option>
+        </select>
+      </div>
+
+      <div className="book-cards">
+        <div className="row">
+          <div className="col">
+            <div className="card-image">
+              <img
+                src="https://books.google.com/books/content?id=GZAoAQAAIAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
+                alt="book"
+              />
+            </div>
+            <p>
+              <b>Title</b>: {t("title.title1")}
+            </p>
+            <p>
+              <b>Author</b>: {t("author.author1")}
+            </p>
+            <p>{t("description.description1")}</p>
+          </div>
+          <div className="col">
+            <div className="card-image">
+              <img
+                src="http://books.google.com/books/content?id=iYOLzgEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
+                alt="book"
+              />
+            </div>
+            <p>
+              <b>Title</b>: {t("title.title2")}
+            </p>
+            <p>
+              <b>Author</b>: {t("author.author2")}
+            </p>
+            <p>{t("description.description2")}</p>
+          </div>
+          <div className="col">
+            <div className="card-image">
+              <img
+                src="http://books.google.com/books/content?id=yFy2MvBbrr4C&printsec=frontcover&img=1&zoom=1&source=gbs_api"
+                alt="book"
+              />
+            </div>
+            <p>
+              <b>Title</b>: {t("title.title3")}
+            </p>
+            <p>
+              <b>Author</b>: {t("author.author3")}
+            </p>
+            <p>{t("description.description3")}</p>
+          </div>
+          <div className="col">
+            <div className="card-image">
+              <img
+                src="http://books.google.com/books/content?id=FIS8swEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
+                alt="book"
+              />
+            </div>
+            <p>
+              <b>Title</b>: {t("title.title4")}
+            </p>
+            <p>
+              <b>Author</b>: {t("author.author4")}
+            </p>
+            <p>{t("description.description4")}</p>
+          </div>
+          <div className="col">
+            <div className="card-image">
+              <img
+                src="http://books.google.com/books/content?id=ZlU3DwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+                alt="book"
+              />
+            </div>
+            <p>
+              <b>Title</b>: {t("title.title5")}
+            </p>
+            <p>
+              <b>Author</b>: {t("author.author5")}
+            </p>
+            <p>{t("description.description5")}</p>
+          </div>
+          <div className="col">
+            <div className="card-image">
+              <img
+                src="http://books.google.com/books/content?id=Zc4YyAEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
+                alt="book"
+              />
+            </div>
+            <p>
+              <b>Title</b>: {t("title.title6")}
+            </p>
+            <p>
+              <b>Author</b>: {t("author.author6")}
+            </p>
+            <p> {t("description.description6")}</p>
+          </div>
+        
+        </div>
+        <div className="row">
+          <div className="col">
+            <div className="card-image">
+              <img
+                src="http://books.google.com/books/content?id=FIS8swEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
+                alt="book"
+              />
+            </div>
+            <p>
+              <b>Title</b>: {t("title.title4")}
+            </p>
+            <p>
+              <b>Author</b>: {t("author.author4")}
+            </p>
+            <p>{t("description.description4")}</p>
+          </div>
+          <div className="col">
+            <div className="card-image">
+              <img
+                src="http://books.google.com/books/content?id=ZlU3DwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+                alt="book"
+              />
+            </div>
+            <p>
+              <b>Title</b>: {t("title.title5")}
+            </p>
+            <p>
+              <b>Author</b>: {t("author.author5")}
+            </p>
+            <p>{t("description.description5")}</p>
+          </div>
+          <div className="col">
+            <div className="card-image">
+              <img
+                src="http://books.google.com/books/content?id=Zc4YyAEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
+                alt="book"
+              />
+            </div>
+            <p>
+              <b>Title</b>: {t("title.title6")}
+            </p>
+            <p>
+              <b>Author</b>: {t("author.author6")}
+            </p>
+            <p> {t("description.description6")}</p>
+          </div>
+          <div className="col">
+            <div className="card-image">
+              <img
+                src="https://books.google.com/books/content?id=GZAoAQAAIAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
+                alt="book"
+              />
+            </div>
+            <p>
+              <b>Title</b>: {t("title.title1")}
+            </p>
+            <p>
+              <b>Author</b>: {t("author.author1")}
+            </p>
+            <p>{t("description.description1")}</p>
+          </div>
+          <div className="col">
+            <div className="card-image">
+              <img
+                src="http://books.google.com/books/content?id=iYOLzgEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
+                alt="book"
+              />
+            </div>
+            <p>
+              <b>Title</b>: {t("title.title2")}
+            </p>
+            <p>
+              <b>Author</b>: {t("author.author2")}
+            </p>
+            <p>{t("description.description2")}</p>
+          </div>
+          <div className="col">
+            <div className="card-image">
+              <img
+                src="http://books.google.com/books/content?id=yFy2MvBbrr4C&printsec=frontcover&img=1&zoom=1&source=gbs_api"
+                alt="book"
+              />
+            </div>
+            <p>
+              <b>Title</b>: {t("title.title3")}
+            </p>
+            <p>
+              <b>Author</b>: {t("author.author3")}
+            </p>
+            <p>{t("description.description2")}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Book;

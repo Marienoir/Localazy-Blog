1be7e01a5a3eb8411868219d4d5000456e3cbdce
Marienoir <okosunmaryeghonghon@gmail.com>
2023-02-05 20:31:18 +0100

    Initial commit for the book app

---
+++ book-app/src/book.js
@@ -0,0 +1,20 @@
+import "./App.css";
+import axios from "axios";
+function Book() {
+    console.log('hghffyggghj');
+//   const getUser = async () => {
+//     try {
+//       const response = await axios.get(
+//         "https://www.googleapis.com/books/v1/volumes?q=harry+potter&callback=handleResponse"
+//       );
+//       console.log(response);
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
+  return <div className="App">
+    <h1>jkjhkjh</h1>
+  </div>
+}
+
+export default Book;
